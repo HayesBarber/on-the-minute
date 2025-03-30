a3fcd5cdcd4824038cb455909f8a791418a1cf7c
Hayes Barber <you@example.com>
2025-03-29 20:41:49 -0400

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # On The Minute
 
 Register a callback to execute every minute on the minute.
+
+```javascript
+const scheduler = new OnTheMinute();
+scheduler.registerCallback(() => console.log(new Date().toString()));
+```
--- test/index.mjs
+++ test/index.mjs
@@ -1,9 +1,4 @@
 import { OnTheMinute } from "../src/index.ts";
 
-const callback = () => {
-  console.log(new Date().toString());
-};
-
 const scheduler = new OnTheMinute();
-
-scheduler.registerCallback(callback);
+scheduler.registerCallback(() => console.log(new Date().toString()));
